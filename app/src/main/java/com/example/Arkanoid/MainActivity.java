6e1a2ae23de76a9a424516a7d9ae5666f439d7d1
tarenstrannik <tarenstrannik@gmail.com>
2024-03-22 21:42:26 +0200

    initial commit

---
+++ app/src/main/java/com/example/Arkanoid/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.Arkanoid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.Arkanoid.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'Arkanoid' library on application startup.
+    static {
+        System.loadLibrary("Arkanoid");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'Arkanoid' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
